08abc28048c19cc8f45e52a6e7f353ed691e793c
AmauryJ <AmauryJ@QA-AMAURYJ>
2018-06-19 11:56:53 -0500

    Javadoc

--- PswProject/src/Primes/Prime_3.java
+++ PswProject/src/Primes/Prime_3.java
@@ -3,22 +3,22 @@ package Primes;
 public class Prime_3 {
 
 	public static long output;
-	public static int n = 100;
+	public static int n = 1000;
 
 	
     // Driver Program to test above function
     public static void main(String args[])
     {
-    	long lStartTime = System.nanoTime();
+    	long lStartTime = System.currentTimeMillis();
     	
-        CribadeEratostenesPrimos(n);
+    	cribadeEratostenesPrimos(n);
 
-        long lEndTime = System.nanoTime();
+        long lEndTime = System.currentTimeMillis();
         output = lEndTime - lStartTime;  
-        System.out.println("Elapsed time in milliseconds: " + output / 1000000);	        
+        System.out.println("Elapsed time in milliseconds: " + output);	        
     }
     
-	public static void CribadeEratostenesPrimos(int n)
+	public static void cribadeEratostenesPrimos(int n)
     {
         // Create a boolean array "prime[0..n]" and initialize
         // all entries it as true. A value in prime[i] will
@@ -42,7 +42,7 @@ public class Prime_3 {
         
         
         
-        FindSpecialPrimes(ConvertPrimeArrayBooltoNumber(prime));
+        findSpecialPrimes(convertPrimeArrayBooltoNumber(prime));
          
         // Print all prime numbers
 //        for(int i = 2; i <= n; i++){
@@ -53,7 +53,7 @@ public class Prime_3 {
         
     }
 	
-	public static int[] ConvertPrimeArrayBooltoNumber(boolean prime[]) {
+	public static int[] convertPrimeArrayBooltoNumber(boolean prime[]) {
 		
 		int count = 0;
 		int count2 = 0;
@@ -79,7 +79,22 @@ public class Prime_3 {
 		
 	}
 	
-	public static void FindSpecialPrimes(int prime[]) {
+	/**
+	 * Returns an Image object that can then be painted on the screen. 
+	 * The url argument must specify an absolute {@link URL}. The name
+	 * argument is a specifier that is relative to the url argument. 
+	 * <p>
+	 * This method always returns immediately, whether or not the 
+	 * image exists. When this applet attempts to draw the image on
+	 * the screen, the data will be loaded. The graphics primitives 
+	 * that draw the image will incrementally paint on the screen. 
+	 *
+	 * @param  prime  an absolute URL giving the base location of the image
+	 * @return      the image at the specified URL
+	 * @see         Image
+	 */
+	
+	public static void findSpecialPrimes(int prime[]) {
 		
 		int j = 5;
 		int indexAfterTen = 5;
@@ -92,7 +107,7 @@ public class Prime_3 {
 		
 		for(int i = 5; i<prime.length; i++) {
 			
-			for(j = 5; j < prime.length; j++) {
+			for(j = indexAfterTen; j < prime.length; j++) {
 				
 				prime1 = Integer.toString(prime[i]);
 				prime2 = Integer.toString(prime[j]);
@@ -108,13 +123,15 @@ public class Prime_3 {
 						break;
 					}
 					
-					if(TwoNumbersEqual(prime1, prime2) == true) {
-						if(ifAlreadyExist(prime1, specialPrimes)) {
+					if(twoNumbersEqual(prime1, prime2) == true) {
+						if(!ifAlreadyExist(prime1, specialPrimes)) {
 							specialPrimes[contador] = prime1;
+							contador++;
 						}
 						
-						if(ifAlreadyExist(prime2, specialPrimes)) {
+						if(!ifAlreadyExist(prime2, specialPrimes)) {
 							specialPrimes[contador] = prime2;
+							contador++;
 						}
 					}
 				}	
@@ -122,30 +139,38 @@ public class Prime_3 {
 		}
 		
 	    // Print all prime numbers
+		int specialCounter = 0;
         for(int i = 0; i < specialPrimes.length; i++){
         	
                 if(specialPrimes[i] != null) {
+                	specialCounter++;
                  	System.out.println("Numero primo especial "+specialPrimes[i]);
                 }
-   
         }
+        
+    	System.out.println("Numeros primos especiales encontrados: "+specialCounter);
 		
 	}	
 	
 	
 	
-	public static boolean TwoNumbersEqual(String a, String b) {
+	public static boolean twoNumbersEqual(String a, String b) {
 		
 		int contador = 0;
+		boolean strikePositionNumber[] = new boolean[a.length()];
 		
 		for(int i = 0; i<a.length(); i++) {
 				
 				for(int j = 0; j < b.length(); j++) {
-					
-					if(a.charAt(i) == b.charAt(j)) {
-						contador ++;
-						break;
+					if(strikePositionNumber[j] != true) {
+						
+						if(a.charAt(i) == b.charAt(j)) {
+							contador ++;
+							strikePositionNumber[j] = true;
+							break;
+						}
 					}
+					
 				}
 				
 				if(contador == 0) {
@@ -164,7 +189,7 @@ public class Prime_3 {
 	 public static boolean ifAlreadyExist(String a, String b[]) {
 		
 		for(int i = 0; i<b.length; i++) {
-			if(b[i] == a) {
+			if(a.equals(b[i])) {
 				return true;
 			}
 		}
